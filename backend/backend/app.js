957559b86d31f0d4c4e99907b23467fefcf5528b
yagomonteir01 <yagomonteirodoc@gmail.com>
2025-08-13 16:00:21 -0300

    Update app.js

--- backend/backend/app.js
+++ backend/backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const routes = require('./routes');  // Importa as rotas
+
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Backend rodando!');
-});
+app.use(express.json());
+app.use(routes);  // Usa as rotas definidas
 
 app.listen(PORT, () => {
-  console.log(`Servidor ativo em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
